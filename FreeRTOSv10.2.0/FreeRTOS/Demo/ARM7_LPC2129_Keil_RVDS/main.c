e6e68d3a0817c7b08ab92fc8be33da80021f0a55
Piotr Ziebinski <piotr.ziebinski97@gmail.com>
2019-03-10 17:32:12 +0100

    3.1/zad3

--- FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -2,29 +2,39 @@
 #include "task.h"
 #include "led.h"
 
+struct BlinkParams {
+	unsigned char ucFrequency;
+	unsigned char ucLedIndex;
+} sBlinkParams;
+
 void LedBlink( void *pvParameters ){
 
 	while(1){
-		Led_Toggle(0);
-		vTaskDelay((1000/(*((unsigned char*)pvParameters)))/2);
+		Led_Toggle((*((struct BlinkParams*)pvParameters)).ucLedIndex);
+		vTaskDelay((1000/((*((struct BlinkParams*)pvParameters)).ucFrequency))/2);
 	}
 }
 
 void LedCtrl( void *pvFreqParam ){
 	
 	while(1){
-		*(unsigned char*)pvFreqParam = *((unsigned char*)pvFreqParam) + 1;
+		(*((struct BlinkParams*)pvFreqParam)).ucFrequency = (*((struct BlinkParams*)pvFreqParam)).ucFrequency + 1;
+		vTaskDelay(1000);
+		(*((struct BlinkParams*)pvFreqParam)).ucFrequency = (*((struct BlinkParams*)pvFreqParam)).ucFrequency + 1;
+		if((*((struct BlinkParams*)pvFreqParam)).ucLedIndex == 3) (*((struct BlinkParams*)pvFreqParam)).ucLedIndex = 0;
+		else	(*((struct BlinkParams*)pvFreqParam)).ucLedIndex = (*((struct BlinkParams*)pvFreqParam)).ucLedIndex + 1;
 		vTaskDelay(1000);
 	}
 }
 
 int main( void ) {
 
-	unsigned char ucBlinkingFreq = 1;
+	sBlinkParams.ucFrequency = 1;
+	sBlinkParams.ucLedIndex = 0;
 	
 	Led_Init();
-	xTaskCreate(LedBlink, NULL , 100 , &ucBlinkingFreq, 1 , NULL );
-	xTaskCreate(LedCtrl, NULL , 100 , &ucBlinkingFreq, 1 , NULL );
+	xTaskCreate(LedBlink, NULL , 100 , &sBlinkParams, 1 , NULL );
+	xTaskCreate(LedCtrl, NULL , 100 , &sBlinkParams, 2 , NULL );
 	vTaskStartScheduler();
 
 	while(1) {};
