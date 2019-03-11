9d280a14f695b26a8172fd1b19f18491f7ca0dae
Piotr Ziebinski <piotr.ziebinski97@gmail.com>
2019-03-11 18:06:57 +0100

    4.1.1/zad3

--- FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -14,6 +14,17 @@ void Pulse_LED0( void *pSemaphore ){
 	}
 }
 
+void Pulse_LED1( void *pSemaphore ){
+
+	while(1){
+		if(xSemaphoreTake(*((xSemaphoreHandle*)pSemaphore), portMAX_DELAY) == pdTRUE) {
+			Led_Toggle(1);
+			vTaskDelay(100);
+			Led_Toggle(1);
+		}
+	}
+}
+
 void PulseTrigger( void *pSemaphore ){
 	
 	vTaskDelay(1000/3);
@@ -31,6 +42,7 @@ int main( void ) {
 	Led_Init();
 	vSemaphoreCreateBinary(xSemaphore);
 	xTaskCreate(Pulse_LED0, NULL , 100 , &xSemaphore , 2 , NULL );
+	xTaskCreate(Pulse_LED1, NULL , 100 , &xSemaphore , 2 , NULL );
 	xTaskCreate(PulseTrigger, NULL , 100 , &xSemaphore, 1 , NULL );
 	vTaskStartScheduler();
 
