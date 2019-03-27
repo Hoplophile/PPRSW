d8255c22cd19ed6725ffae42b18c3a73d2469bf9
Piotr Ziebinski <piotr.ziebinski97@gmail.com>
2019-03-27 14:12:23 +0100

    5.1/zad1

--- FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOSv10.2.0/FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -1,55 +1,18 @@
 #include "FreeRTOS.h"
 #include "task.h"
-#include "keyboard.h"
-#include "serwo.h"
+#include "led.h"
+#include "timer_interrupts.h"
 
-enum KeyboardState ePreviousState = RELASED, eCurrentState = RELASED;
-
-void Keyboard (void *pvParameters){
+void LedBlink(void) {
 	
-	while(1){			
-		ePreviousState = eCurrentState;
-		eCurrentState = eKeyboard_Read();
-		
-		if (ePreviousState != eCurrentState){
-			switch(eCurrentState){
-				case BUTTON_1: 
-					Servo_Callib();
-					break;
-				case BUTTON_2: 
-					Servo_GoTo(50);
-					break;
-				case BUTTON_3: 
-					Servo_GoTo(100);
-					break;
-				case BUTTON_4: 
-					Servo_Speed(8);
-					Servo_GoTo(12);
-					Servo_Wait(100);
-					Servo_Speed(4);
-					Servo_GoTo(24);
-					Servo_Wait(100);
-					Servo_Speed(2);
-					Servo_GoTo(36);
-					Servo_Wait(100);
-					Servo_Speed(1);
-					Servo_GoTo(0);
-					break;
-				default:
-					break;
-			}		
-		}
-		vTaskDelay(1);
-	}
+	Led_Toggle(0);
 }
 
-int main( void ){
+int main(void) {
 	
-	ButtonInit();
-	Servo_Init();
-	
-	xTaskCreate(Automat, NULL, 127, NULL, 1, NULL);
-	xTaskCreate(Keyboard, NULL, 127, NULL, 2, NULL);
+	Led_Init();
+	Timer1Interrupts_Init(500, &LedBlink);
 	vTaskStartScheduler();
-	while(1){};
+	
+	while(1) {};
 }
